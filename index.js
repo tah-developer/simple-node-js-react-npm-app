10925cdf3b857cadf22179daf52d3ae23a17ef78
Tristan Hall <tristan@cloudbasesoftware.uk>
2024-03-28 19:56:00 +0000

    simplify app and add start script

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use("/api", Router);
+app.get("/", (req, res) => res.send("<h1>Hello from jenkins!</h1>"));
+app.listen(3000, () => console.log("app listening on port 3000"));
+// app.use(express.json());
+// app.use("/api", Router);
 
-async function startServer() {
-  try {
-    await connectDb();
-    app.listen(process.env.PORT, () =>
-      console.log("App is listening on port", process.env.PORT)
-    );
-  } catch (error) {
-    console.error("Something went wrong", error);
-  }
-}
+// async function startServer() {
+//   try {
+//     await connectDb();
+//     app.listen(process.env.PORT, () =>
+//       console.log("App is listening on port", process.env.PORT)
+//     );
+//   } catch (error) {
+//     console.error("Something went wrong", error);
+//   }
+// }
 
-startServer();
+// startServer();
